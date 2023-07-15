e58a93ed4f1adc2eeb09f2a1e2d806618f920e23
masayoshikob <masayoshikob@gmail.com>
2023-07-15 18:21:35 +1200

    prep release v0.0.2a

---
+++ prebuild/v0.0.2a/win/BitnetConsole.cmd
@@ -0,0 +1,21 @@
+@echo off
+
+:: Copyright 2023 Bitnet
+:: This file is part of the Bitnet library.
+::
+:: This software is provided "as is", without warranty of any kind,
+:: express or implied, including but not limited to the warranties
+:: of merchantability, fitness for a particular purpose and
+:: noninfringement. In no even shall the authors or copyright
+:: holders be liable for any claim, damages, or other liability,
+:: whether in an action of contract, tort or otherwise, arising
+:: from, out of or in connection with the software or the use or
+:: other dealings in the software.:: Copyright 2023 Bitnet
+:: This file is part of the Bitnet library.
+::
+:: This script initiates the Javascript console that allows you
+:: to control your node.
+
+title Bitnet Javascript Console
+color 07
+.\bitnet attach \\.\pipe\bitnet
